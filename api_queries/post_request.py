1aaff7551d72751efe525ea56e8c4b4c01bda4a5
Alakbar Mammadov <a.mammadov@fugro.com>
2022-12-12 00:13:57 +0100

    updating docker-compose.yml

--- api_queries/post_request.py
+++
@@ -1,6 +0,0 @@
-import requests, json
-url = "https://api.elbrusgroup.net/chargers"
-post_payload = {"City":"Korsa","Street":"Korsveien 3","PostCode":"19021","SN":"MD129F643H","id":"1","Model":"c12a","Output":"22"}
-post_response = requests.post(url, params=post_payload)
-print(post_response.text)
-
